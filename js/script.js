45c92bda79bf16a2b84550795fde2095a5a79e74
= <=>
2022-01-09 19:20:33 -0500

    Add logic to update year using js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+const $copyrightText = document.querySelector('.copyright-text')
+const year = new Date().getFullYear()
+
+$copyrightText.textContent = `Copyright ${year} Enes Tasbasi. All right reserved`
